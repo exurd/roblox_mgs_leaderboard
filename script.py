ba5f8f6085159a8c8dbcf6a42c8404c35fb81410
exurd <7785022+exurd@users.noreply.github.com>
2024-01-16 15:37:10 +0000

    improve: reqSession, checks, verbose printing and condensation
    - gave the script a trackable user agent
    - fix not getting users without a title=""
    - added checks if {} occurs, fixes #1
    - added verbose printing for issue #2
    - condensed the bottom end of the script to a loop

--- script.py
+++ script.py
@@ -7,6 +7,12 @@ import json
 # the art of bodging doesn't care about beauty
 # IT WILL BE JANKY AND YOU ARE GONNA LIKE IT
 
+requestSession = requests.Session()
+adapter = requests.adapters.HTTPAdapter(max_retries=5)
+requestSession.mount('https://', adapter)
+requestSession.mount('http://', adapter)
+requestSession.headers['User-Agent'] = "roblox_mgs_leaderboard github action"
+
 def extractTable(table):
     data_list = []
 
@@ -14,14 +20,17 @@ def extractTable(table):
 
     for row in rows[1:]:
         cells = row.find_all('td')
+        #print(f"cells: {cells}") # a bit too verbose but just in case
         rank = cells[0].get_text(strip=True)
 
         user_link = ""
 
-        user_element = cells[1].find('a', href=True, title=True)
+        # reminder, there are hidden accounts. check in incognito mode in case the user is not found due to them being a hidden account (takes 1 minute, saves 1 hour)
+        user_element = cells[1].find_all('a', attrs={'href': True, 'name': True})
+        print(f"user_element: {user_element}")
         if user_element:
-            user_link = user_element['href']
-        
+            user_link = user_element[0]['href']
+
         score = cells[2].get_text(strip=True).replace('\u202f', '')
 
         data_list.append([rank, user_link, score])
@@ -35,7 +44,7 @@ def getMetaGamerScore_leaderboard_stats(type):
         #pageNum = 1
         #print(x)
         url = f"https://metagamerscore.com/platform_toplist/roblox/{str(type)}?page={x}"
-        req = requests.get(url)
+        req = requestSession.get(url)
 
         if req.ok:
             soup = BeautifulSoup(req.text, 'html.parser')
@@ -47,6 +56,10 @@ def getMetaGamerScore_leaderboard_stats(type):
                     "mgs_link": user_link,
                     "score": score
                 }
+
+    if data_dict == {}: # page error?
+        print("Nothing has been found; something went completely wrong")
+        return False
     return data_dict
 
 #print(data_dict)
@@ -56,12 +69,15 @@ def getUserIds(data_dict):
     for rank in data_dict:
         mgs_link = data_dict[rank]['mgs_link']
         if mgs_link == "":
+            print("No MGS profile; profile hidden from public / not found when importing table...")
             data_dict[rank]['roblox_id'] = None
             continue
-        url = f"https://metagamerscore.com{data_dict[rank]['mgs_link']}?tab=accounts"
+        url = f"https://metagamerscore.com{mgs_link}?tab=accounts"
         #print(url)
 
-        req = requests.get(url)
+        req = requestSession.get(url)
+
+        print(f"Status Code {req.status_code}")
 
         if req.ok:
             soup = BeautifulSoup(req.text, 'html.parser')
@@ -72,6 +88,7 @@ def getUserIds(data_dict):
             
             if profile_links == []:
                 data_dict[rank]['roblox_id'] = None
+                print("No Roblox User ID detected; profile hidden their gaming accounts from public...")
                 continue
 
             for link in profile_links:
@@ -82,42 +99,47 @@ def getUserIds(data_dict):
 
                 if match:
                     user_id = match.group(1)
-                    #print("User ID:", user_id)
+                    print(f"{str(mgs_link)} is Roblox User ID {str(user_id)}")
                     data_dict[rank]['roblox_id'] = int(user_id)
                     time.sleep(.75)
                     continue
                 else:
-                    print(f"No match found for {link['href']}")
+                    print(f"No match found for {str(mgs_link)}")
+        else:
+            print("Request is not ok! Printing text:")
+            print(req.text)
+
+testing = False
 
 # https://metagamerscore.com/platform_toplist/roblox/score
-score_dict = getMetaGamerScore_leaderboard_stats("score")
-getUserIds(score_dict)
-
-json_file_path = 'score_rblx.json'
-
-with open(json_file_path, 'w') as json_file:
-    json.dump(score_dict, json_file, indent=4)
-
-print(f'Data saved to {json_file_path}')
-
 # https://metagamerscore.com/platform_toplist/roblox/completist
-completist_dict = getMetaGamerScore_leaderboard_stats("completist")
-getUserIds(completist_dict)
-
-json_file_path = 'completist_rblx.json'
-
-with open(json_file_path, 'w') as json_file:
-    json.dump(completist_dict, json_file, indent=4)
-
-print(f'Data saved to {json_file_path}')
-
 # https://metagamerscore.com/platform_toplist/roblox/firsts
-firsts_dict = getMetaGamerScore_leaderboard_stats("firsts")
-getUserIds(firsts_dict)
 
-json_file_path = 'firsts_rblx.json'
+categories = ["score", "completist", "firsts"]
 
-with open(json_file_path, 'w') as json_file:
-    json.dump(firsts_dict, json_file, indent=4)
+for category in categories:
+    print(f"Getting category [{category}]...")
+    toplist_dict = getMetaGamerScore_leaderboard_stats(category)
+    if toplist_dict == False:
+        print(toplist_dict)
+    else:
+        print("Grabbed list successfully!")
 
-print(f'Data saved to {json_file_path}')
+        print("Dumping JSON as of now to console...")
+        print(toplist_dict)
+
+        print(f"Getting User IDs from {category}...")
+        getUserIds(toplist_dict)
+
+        print("Dumping finished JSON to console...")
+        print(toplist_dict)
+
+        json_file_path = f'{category}_rblx.json'
+        print("Saving data...")
+        if not testing:
+            with open(json_file_path, 'w') as json_file:
+                json.dump(toplist_dict, json_file, indent=4)
+                print(f'Data saved to {json_file_path}')
+                json_file.close()
+        else:
+            print("Testing mode; not saving (if this is on actions tell the village idiot to make that value False!)")
